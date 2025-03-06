deae449bf7cf8294f4af51fc302669c2bc763bff
akash100a <arakash968786@gmail.com>
2025-03-06 14:34:22 +0600

    Refactor navbar structure and styles: update HTML for improved layout, enhance CSS for better responsiveness, and add JavaScript for toggle functionality

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+let navbarButton = document.querySelector('.navabar_toggler');
+let navbarCollapse = document.querySelector('.navbar_collapse');
+
+
+navbarButton.addEventListener('click', function() {
+    navbarCollapse.classList.toggle('showNav');
+});

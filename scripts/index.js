c54f295f3aafbedd78de994c4f6250d7356ace8e
akash100a <arakash968786@gmail.com>
2025-03-07 14:38:38 +0600

    Add load more button functionality and style: implement event listener for toggling active state and enhance button design with new styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,12 @@
 let navbarButton = document.querySelector('.navabar_toggler');
 let navbarCollapse = document.querySelector('.navbar_collapse');
-
+let loadMoreButton = document.querySelector('.testimonial .container .load_more button');
 
 navbarButton.addEventListener('click', function() {
     navbarCollapse.classList.toggle('showNav');
     navbarButton.classList.toggle('show');
+});
+
+loadMoreButton.addEventListener('click', function() {
+    loadMoreButton.classList.toggle('active');
 });

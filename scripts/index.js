50557d8e146476320b7769a5ba1549920f9af5c7
akash100a <arakash968786@gmail.com>
2025-03-07 14:44:32 +0600

    Add functionality to load more testimonials: implement event listener to activate testimonial inner section on button click and clean up CSS

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,17 @@
-let navbarButton = document.querySelector('.navabar_toggler');
-let navbarCollapse = document.querySelector('.navbar_collapse');
-let loadMoreButton = document.querySelector('.testimonial .container .load_more button');
+let navbarButton = document.querySelector(".navabar_toggler");
+let navbarCollapse = document.querySelector(".navbar_collapse");
+let loadMoreButton = document.querySelector(
+  ".testimonial .container .load_more button"
+);
+let testimonial_inner = document.querySelector(
+  ".testimonial .container .testimonial_grid .testimonial_inner"
+);
 
-navbarButton.addEventListener('click', function() {
-    navbarCollapse.classList.toggle('showNav');
-    navbarButton.classList.toggle('show');
+navbarButton.addEventListener("click", function () {
+  navbarCollapse.classList.toggle("showNav");
+  navbarButton.classList.toggle("show");
 });
 
-loadMoreButton.addEventListener('click', function() {
-    loadMoreButton.classList.toggle('active');
+loadMoreButton.addEventListener("click", function () {
+  testimonial_inner.classList.add("active");
 });
